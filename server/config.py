176b158d2cb8271f5507f170f63dda75d2d8209a
AnhNguyen <hung000anh@gmail.com>
2025-10-14 17:18:05 +0700

    refactor file path model ml to dl

--- server/config.py
+++ server/config.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parents[0]
 MODEL_DIR = Path(os.getenv("MODEL_DIR", BASE_DIR / "models"))
-DEFAULT_MODEL = os.getenv("DEFAULT_MODEL", "selfcontained_logreg.joblib")
+DEFAULT_MODEL = os.getenv("DEFAULT_MODEL", "model-textcnn-lstm.h5")
+DEFAULT_TOKENIZER = os.getenv("DEFAULT_TOKENIZER", "tokenizer.pkl")
 MODEL_PATH = MODEL_DIR / DEFAULT_MODEL
+TOKENIZER_PATH = MODEL_DIR / DEFAULT_TOKENIZER
 
 CERTS_DIR = os.getenv("CERTS_DIR", BASE_DIR / "certs")
 SSL_FILE = os.getenv("SSL_FILE", CERTS_DIR / "prod-ca-2021.crt")
